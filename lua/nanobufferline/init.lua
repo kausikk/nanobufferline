34fef515211cf62f33725b1fb575fc4903079857
kausikk <kausikkris10@gmail.com>
2025-12-14 16:47:40 -0800

    Add setup function to set current buffer highlight

--- lua/nanobufferline/init.lua
+++ lua/nanobufferline/init.lua
@@ -7,6 +7,10 @@ columns = {}
 lastmark = -1
 ns_id = vim.api.nvim_create_namespace("_nbl")
 
+M.setup(opts) = function(opts)
+	M.hl_group = opts.hl_group
+end
+
 M._change_buffer = function(ev)
 	local ibuf = 0 
 	for i, buf in ipairs(buffers) do
